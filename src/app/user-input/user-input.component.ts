ecb9575e7d7a40ab57f039360cc5fffa2ee8ba7b
Igor Antonov <igor.antonov@studiodesigner.com>
2025-06-05 05:14:12 -0400

    Initial commit

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  enteredInitialInvestment = '0';
+
+  onSubmit(){
+    // Handle form submission logic here
+    console.log('Form submitted');
+  }
+}
