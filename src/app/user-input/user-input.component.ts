2443cb3a1033e66fed603204e5df475955d0bd69
Igor Antonov <igorantonoff@gmail.com>
2025-06-20 06:26:56 -0400

    User inputs enhancements

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -11,9 +11,16 @@ import { FormsModule } from '@angular/forms';
 export class UserInputComponent {
 
   enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
 
   onSubmit(){
     // Handle form submission logic here
     console.log('Form submitted');
+    console.log('Initial Investment:', this.enteredInitialInvestment);
+    console.log('Annual Investment:', this.enteredAnnualInvestment);  
+    console.log('Expected Return:', this.enteredExpectedReturn);
+    console.log('Duration:', this.enteredDuration);
   }
 }
